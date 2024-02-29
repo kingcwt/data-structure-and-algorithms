11a501077006a18240a0f0b13b267b8d3275f119
kingcwt <kingcwt@qq.com>
2024-02-29 14:45:28 +0800

    feat: 求雨水 注释 动态规划求解接雨水问题

--- 42/index.js
+++ 42/index.js
@@ -24,3 +24,35 @@ var trap = function (height) {
 
 
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+
+
+
+
+// 动态规划
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+
+  
+    let n = height.length;
+    if (n < 1) return [];
+    let leftMax = new Array(n).fill(0);
+    let rightMax = new Array(n).fill(0);
+
+    leftMax[0] = height[0];
+    for(let i = 1; i < n; i++){
+        leftMax[i] = Math.max(leftMax[i - 1],height[i])
+    }
+    rightMax[n - 1] = height[n - 1]
+    for(let i = n - 2; i >=0; i--){
+        rightMax[i] = Math.max(rightMax[i + 1],height[i])
+    }
+
+    let ans = 0;
+    for(let i = 1; i < n; i++){
+        ans += Math.min(leftMax[i],rightMax[i]) - height[i]
+    }
+    return ans;
+};
